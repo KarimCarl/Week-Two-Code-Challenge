29155914a3f73c2ac885e95faec5d14c88c363df
KarimCarl <cndungu109@gmail.com>
2023-02-20 09:53:22 +0300

    Created a few methods within Author class

--- lib/Author.rb
+++ lib/Author.rb
@@ -8,17 +8,22 @@ class Author
     def initialize(name)
         @name = name
     end
+
     def articles
         Article.all.filter do |article|
           article.author == @name
-        end
     end
+
+    end
+    
     def magazines
         articles.map{|article|article.magazine}.uniq
     end
+
     def add_article(magazine, title)
         Article.new(self, magazine, title)
     end
+
     def topic_areas
         magazines.map{|magazine| magazine.category}.uniq
     end
